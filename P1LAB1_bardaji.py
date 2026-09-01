798352ddf8de4d185d34cf43776f9be1e17ae315
johnryan0 <johnbardaji0@gmail.com>
2026-09-01 18:39:57 +0000

    P1LAB1 finished

--- P1LAB1_bardaji.py
+++ P1LAB1_bardaji.py
@@ -6,3 +6,15 @@
 
 # First, we're going to greet the user 
 print("Hello!")
+
+# Next, ask their first name
+print("what's your name? ")
+first_name = input()
+
+# then say back the name
+print("Nice to meet you", first_name)
+
+print("what's your last name", first_name,"?")
+last_name = input()
+
+print("Hello, ", first_name, last_name)
