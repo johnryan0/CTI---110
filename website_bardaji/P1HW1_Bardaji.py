b31de9608852e989a6f36c9586c440924d0850be
johnryan0 <johnbardaji0@gmail.com>
2026-09-08 18:17:54 +0000

    P1HW1 final final version

--- website_bardaji/P1HW1_Bardaji.py
+++ website_bardaji/P1HW1_Bardaji.py
@@ -5,9 +5,14 @@
 
 
 # PART 1 - EXPONENTS
-
+base = int(input("Enter integer as base value: "))
+exponent = int(input("enter integer as exponent: "))
+result = base ** exponent # example, (3 ** 2) is 3 squared
+print(f"{base}  to the {exponent} power is {result} !!")
 
 # PART 2 - ADDITION SUBTRACTION 
+print("-----Addition and Subtraction-----")
+print("\n") # 2 newlines 
 # 3 numbers, start, add this, sub this
 start = int(input("Enter the starting integer: "))
 print("you typed", start)
@@ -16,6 +21,6 @@ sub_this = int(input("Enter integer to subtract: "))
 
 answer = start + add_this - sub_this
 print()
-print() # that gives 2 newlines, so would print("\n"
+print() # that gives 2 newlines, so would print("\n")
 print(start, "+", add_this, "-", sub_this, "Is equal to", answer)
 print(f"{start} + {add_this} - {sub_this} is equal to {answer}")
