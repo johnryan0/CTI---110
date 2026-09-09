c07d647e826e7faa87fec9c60f49cbc2ef8fc30c
johnryan0 <johnbardaji0@gmail.com>
2026-09-09 23:13:47 +0000

    P1HW2_Bardajijohn.py

--- P1HW2_Bardaji.py
+++ P1HW2_Bardaji.py
@@ -1,23 +1,3 @@
-# PART 1 - EXPONENTS
-base = int(input("Enter integer as base value: "))
-exponent = int(input("enter integer as exponent: "))
-result = base ** exponent # example, (3 ** 2) is 3 squared
-print(f"{base}  to the {exponent} power is {result} !!")
-
-# PART 2 - ADDITION SUBTRACTION 
-print("-----Addition and Subtraction-----")
-print("\n") # 2 newlines 
-# 3 numbers, start, add this, sub this
-start = int(input("Enter the starting integer: "))
-print("you typed", start)
-add_this = int(input("Enter Integer to add: "))
-sub_this = int(input("Enter integer to subtract: "))
-
-answer = start + add_this - sub_this
-print()
-print() # that gives 2 newlines, so would print("\n")
-print(start, "+", add_this, "-", sub_this, "Is equal to", answer)
-print(f"{start} + {add_this} - {sub_this} is equal to {answer}")
 
 # John Bardaji
 # 9/9/2026
@@ -25,14 +5,31 @@ print(f"{start} + {add_this} - {sub_this} is equal to {answer}")
 # A travel expenses calculator 
 
 #  Ask user to enter their budget
-Budget = float(input("Enter Budget", ))
+Budget = int(input("Enter Budget: ", ))
 
 # Ask user to enter travel destination
-Destination = (input("Enter the travel destination"))
+Destination = (input("Enter your travel destination: "))
 
 #  Ask user for amount they will spend on gas
-Gas = float(input("Enter Gas cost"))
+Gas = int(input("How much do you think you will spend on gas? "))
 
 # Ask user for amount they will spend on accommodation
-Accomidation = float(input("accomodation "))
+Accomodation = int(input("Approximetely, how much will you need for Accomodation/hotel? "))
 
+# Ask user for amount they will spend on food
+Food = int(input("Last, how much will you spend on Food? "))
+
+# Process user input
+Expenses = Gas + Accomodation + Food 
+Results = Budget - Expenses
+
+# Show processed input
+print("----------Travel Expenses----------")
+print("Location:", Destination)
+print("Initial budget:", Budget)
+print()
+print("Fuel:", Gas)
+print("Accomodation:", Accomodation)
+print("Food:", Food)
+print()
+print("Remaining balance:", Results)
